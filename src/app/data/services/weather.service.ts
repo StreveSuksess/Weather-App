a4462da6ffd6298ea93a5c65d9477f1a3edc46e3
StreveSuksess <strevesuksess@gmail.com>
2025-03-14 21:39:54 +0300

    add service

--- src/app/data/services/weather.service.ts
+++ src/app/data/services/weather.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  private http = inject(HttpClient)
+  private baseApiUrl = 'https://api.weatherapi.com/v1/'
 
-  constructor() { }
+
 }
