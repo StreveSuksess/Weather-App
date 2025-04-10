5531c824f70c5a45480920aae9e97b47f5ed553d
StreveSuksess <strevesuksess@gmail.com>
2025-04-10 19:39:37 +0300

    bug fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,13 @@
-export const environment: { production: boolean, apiKey: string } = {
-  production: false,
-  apiKey: '19df4880065b439fbfa103452251303'
+export const environment: {
+  production: boolean,
+  weatherApiKey: string,
+  baseWeatherApiUrl: string,
+  baseSearchCityApiUrl: string,
+  searchCityUsername: string
+} = {
+  production: true,
+  weatherApiKey: '19df4880065b439fbfa103452251303',
+  baseWeatherApiUrl: 'https://api.weatherapi.com/v1/',
+  baseSearchCityApiUrl: 'http://api.geonames.org/',
+  searchCityUsername: 'strevesuksess'
 }
